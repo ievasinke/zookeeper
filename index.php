c6790acb5cc1bd9bbaadee3cee3d7575bfc64978
Ieva Šinke <ieva.sinke@gmail.com>
2024-05-26 08:29:27 +0300

    init

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Create an application Zookeeper where you have defined multiple animals at the start.
+ * Each animal has its happiness value as well as type of food he likes.
+ * When animals play for certain amount of time (5 seconds)
+ * their happiness increases but food reserves gets lower.
+ * When animals need to work, their happiness value drops as well as their food reserves.
+
+ * There should be an option to select an animal and feed it,
+ * as well as pet it in order to increase their happiness level and food reserves.
+ * The food that you give animal must be validated and
+ * if the wrong food is given the animals happiness drops and
+ * food reserves drops twice instead of increasing.
+
+ * Mandatory requirements:
+ * - Standalone git repository
+ * - PSR standards
+ * - At least 2 3rd party packages used from https://packagist.org/
+ */
